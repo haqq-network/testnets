48470262e7bd7b207ed9906db2136246040daf9b
Evgeniy Abramov <5331145+kioqq@users.noreply.github.com>
2023-06-14 17:08:44 +0400

    Update state_sync.sh

--- TestEdge2/state_sync.sh
+++ TestEdge2/state_sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SNAP_RPC="https://rpc.tm.testedge2.haqq.network:443"
+SNAP_RPC1="https://rpc.tm.testedge2.haqq.network:443"
+SNAP_RPC2="https://te2-s1-tm.haqq.sh:443"
 
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
 BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
@@ -10,9 +11,9 @@ TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.bloc
 P_PEERS=""
 
 # seed nodes
-SEEDS="62bf004201a90ce00df6f69390378c3d90f6dd7e@seed2.testedge2.haqq.network:26656,23a1176c9911eac442d6d1bf15f92eeabb3981d5@seed1.testedge2.haqq.network:26656"
+SEEDS="62bf004201a90ce00df6f69390378c3d90f6dd7e@seed2.testedge2.haqq.network:26656,23a1176c9911eac442d6d1bf15f92eeabb3981d5@seed1.testedge2.haqq.network:26656,96cd4df06277f3353fa2da1f73d8e21663183c3f@91.107.192.98:26656"
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC1,$SNAP_RPC2\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
 s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"$P_PEERS\"| ; \
